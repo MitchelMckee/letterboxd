c606bc0f7a9510bf33cfefdd2551a729225b7346
MitchelMckee <79174382+MitchelMckee@users.noreply.github.com>
2023-05-09 21:17:57 +0100

    add chakra and make useable ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import FindFavourites from "./components/FindFavourites.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>
-      <FindFavourites />
-    </div>
+    <ChakraProvider>
+      <Box bg="#202830" h="100vh">
+        <div>
+          <div className="letterboxd-logo" />
+          <FindFavourites />
+        </div>
+      </Box>
+    </ChakraProvider>
   );
 }
 
--- src/components/FindFavourites.jsx
+++ src/components/FindFavourites.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Flex, Input, Heading, Button, Image } from "@chakra-ui/react";
+import "./styles.css";
 
 function UsernameForm() {
   const [username, setUsername] = useState("");
@@ -19,15 +21,65 @@ function UsernameForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Letterboxd Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
+      <Flex justifyContent="center" alignItems="center">
+        <Image
+          src={require("../assets/img/letterboxd-dots.png")}
+          h={"120px"}
+          objectFit={"contain"}
+          alt={"Letterboxd Logo"}
+          paddingTop={"2%"}
+          paddingBottom={"2%"}
         />
+      </Flex>
+      <label>
+        <Heading
+          as="h1"
+          id="title"
+          fontWeight={"bold"}
+          textAlign={"Center"}
+          color={"white"}
+          paddingBottom={"2%"}
+        >
+          Letterboxd Favourites Quirk'o'meter
+        </Heading>
+        <Heading
+          as={"h2"}
+          fontSize={"25"}
+          color={"white"}
+          textAlign={"Center"}
+          paddingBottom={"1%"}
+        >
+          Enter your Letterboxd username*
+        </Heading>
+        <Flex justifyContent="center" alignItems="center">
+          <Input
+            width={"20%"}
+            bg={"FFFFFF"}
+            placeholder="Type here..."
+            variant={"filled"}
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            id="search-bar"
+          />
+        </Flex>
       </label>
-      <button type="submit">Search</button>
+      <Flex
+        justifyContent={"center"}
+        alignItems={"center"}
+        paddingTop={"2%"}
+        paddingBottom={"2%"}
+      >
+        <Button colorScheme={"green"} type="submit">
+          See how quirky you really are!
+        </Button>
+      </Flex>
+
+      <Heading as={"h3"} color={"white"} textAlign={"Center"} size={"16"}>
+        *This isn't always your display name, if your display name isn't
+        <br />
+        working go to settings and use whatever comes after “Signed in as”
+      </Heading>
       {favourite && (
         <ul>
           {favourite.map((film, index) => (
