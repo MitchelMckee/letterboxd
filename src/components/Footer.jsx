52c485381b8b1ee8a6f81f0036787a0fd960e227
MitchelMckee <79174382+MitchelMckee@users.noreply.github.com>
2024-01-21 14:54:48 +0000

    update github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
     <footer>
-      <a href="https://github.com/MitchelMckee">
+      <a href="https://github.com/MitchelMckee/letterboxd">
         <FontAwesomeIcon
           icon={faGithub}
           color="white"
